df09d2e88500b63faed5600d9cfd2f8bd026b3f7
abdotha <abdotha5@gmail.com>
2024-04-29 01:28:03 +0300

    new

---
+++ header/Error_handling.h
@@ -0,0 +1,6 @@
+#include"REG52.H"
+void errorDis(unsigned char Msg[]);
+unsigned char divByZeroError(unsigned char oprand2,unsigned char opration);
+unsigned char invldOpr(unsigned char dig1,unsigned char dig2,unsigned char dig3);
+unsigned char invldOpration(unsigned char oprator);
+void DELAY(unsigned int time);
---
+++ src/conversion.c
@@ -0,0 +1,21 @@
+#include"header\conversion.h"
+#include"header\lcd.h"
+int hex_to_dec(unsigned char dig_1,unsigned char dig_2,unsigned char dig_3)
+{
+    int dec_num;
+    dig_1=(dig_1-'0');
+    dig_2=(dig_2-'0');
+    dig_3=(dig_3-'0');
+    dec_num=(dig_1*100)+(dig_2*10)+dig_3;
+    return dec_num;
+}
+
+void dec_to_hex(int dec,unsigned char *hex)
+{
+    unsigned char x;
+    for(x=0;x<=2;x++)
+    {
+        hex[x]=(dec%10)+'0';
+        dec=dec/10;
+    }
+}
